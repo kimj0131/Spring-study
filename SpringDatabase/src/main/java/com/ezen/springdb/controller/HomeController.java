be7358490877d20058e31f4af907a7b4f95ca5ab
junhyoungkim <kkjn0131@naver.com>
2024-01-16 10:52:37 +0900

    log4jdbc 인터페이스 사용해보기 >> jdbc에 발생하는 작업들을 로그로 찍어준다

--- SpringDatabase/src/main/java/com/ezen/springdb/controller/HomeController.java
+++ SpringDatabase/src/main/java/com/ezen/springdb/controller/HomeController.java
@@ -12,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.ezen.springdb.dto.EmployeeDTO;
 import com.ezen.springdb.mapper.EmployeeMapper;
 
 @Controller
@@ -27,12 +26,14 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 
+		empMapper.getAll();
+		
 //		log.info(empMapper.getAll());
 		
-		for(EmployeeDTO emp : empMapper.getAll()) {
-			System.out.println(emp);
-		}
-		
+//		for(EmployeeDTO emp : empMapper.getAll()) {
+//			System.out.println(emp);
+//		}
+//		
 
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
